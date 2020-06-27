39ccdf6d4980e6bf691b84881014d83397f567e2
alali004 <alali62004@gmail.com>
2020-06-27 20:56:43 +0300

    تم الحل

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const name= "ali";
+const Age= 15;
+console.log("انا"+name+"عمري"+Age);
+
+console.log(Age*5);
+console.log(5);
+
+const hobbies=["videogames", "Football"];
+hobbies.push(15);
+console.log(hobbies);
+hobbies.pop();
+console.log(hobbies);
+let ali={
+track:"web",
+lang:"HTML",
+teachers:["hussain", "moudhi", "rahaf", "nancy"]
+};
+console.log(ali.track);
+console.log(ali.teachers[0]);
